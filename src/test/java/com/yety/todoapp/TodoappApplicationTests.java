ea25ee4ae9bda48127b5d075f0803f8b1b0f6e06
Yesid R <yety93@hotmail.com>
2022-07-06 23:29:11 -0500

    Creación base del proyecto todo_app

---
+++ src/test/java/com/yety/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yety.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
